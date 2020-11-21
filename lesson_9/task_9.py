c9a64ffdb1f29ff560e02f41e357969b8ac1adc5
Vitalii <parf.vetal@gmail.com>
2020-11-21 03:18:42 +0200

    added code to task1_2

--- lesson_9/task_9.py
+++ lesson_9/task_9.py
@@ -1,17 +1,16 @@
 def task1():
     with open('myfile.txt', 'w') as myfile:
-        myfile.write('Hello file world')
-
-
-
+        myfile.write('Hello file world.\n')
 
 
 def task1_2():
     with open('myfile.txt', 'r') as myfile:
         read_myfile = myfile.read()
-    print(read_myfile)
+        print(read_myfile)
 
-
+    with open('e:\\myfile.txt', 'r') as myfile2:
+        read_myfile2 = myfile2.read()
+        print(read_myfile2)
 
 
 def task2():
