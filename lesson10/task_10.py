3137daed278206ce22433cc8e44af1d004f70e2e
Vitalii <parf.vetal@gmail.com>
2020-11-26 17:07:55 +0200

    added lesson10 branch

---
+++ lesson10/task_10.py
@@ -0,0 +1,34 @@
+class Person:
+
+    def __init__(self, firstname: str, lastname: str, age: int):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.age = age
+
+    def talk(self):
+        return f"Hello, my name is {self.firstname} {self.lastname} and i'm {self.age} years old"
+
+
+class Dog:
+
+    age_factor = 7
+
+    def __init__(self, dog_age: int):
+        self.dog_age = dog_age
+
+    def human_age(self):
+        return f'Dog age in human equivalent is {self.age_factor * self.dog_age}'
+
+
+# class TvController:
+#     pass
+
+
+person1 = Person('Carl', 'Jonson', 26)
+person2 = Person('Mr.', 'Morgan', 50)
+dog_sharik = Dog(5)
+dog_bobik = Dog(3)
+
+
+
+
--- main.py
+++ main.py
@@ -1,2 +1,10 @@
+from lesson10 import task_10 as t10
 
 
+print(t10.person1.talk())
+
+print(t10.person2.talk())
+
+print(t10.dog_bobik.human_age())
+
+print(t10.dog_sharik.human_age())
