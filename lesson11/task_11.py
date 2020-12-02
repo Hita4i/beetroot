0fcb38ac423a1a9fc693e5b010de38e13c5d454f
Vitalii <parf.vetal@gmail.com>
2020-12-02 12:26:07 +0200

    Fix

--- lesson11/task_11.py
+++ lesson11/task_11.py
@@ -7,7 +7,7 @@ class Person:
         self.age = age
 
     def __repr__(self):
-        return self.first_name + ' ' + self.last_name + ' - ' + str(self.age)
+        return f'{self.first_name} {self.last_name} - {self.age}'
 
 
 class Student(Person):
@@ -28,9 +28,8 @@ class Teacher(Person):
         self.salary = salary
 
     def __repr__(self):
-        return (self.first_name + ' ' + self.last_name + '\n' + str(self.age) + ' ' + 'Years old' + '\n' +
-                self.subject + '\n' + str(self.student_count) + '\n' +
-                str(self.salary))
+        return f'{self.first_name} {self.last_name} {self.age} Years old\nSusject is: {self.subject} \n' +\
+                    f'Student count is: {self.student_count}\nSalsry: {self.salary}'
 
 
 person = Person('Marusia', 'Marusevich', 23)
@@ -43,23 +42,20 @@ print(person)
 
 class Mathematician:
 
-    def square_nums(self, *args):
-        self.list_of_integer = args
-        return [i * i for i in args]
+    def square_nums(self, items):
 
-    def remove_positives(self, *args):
-        self.list_of_numbers = args
-        return [i for i in args if i < 0]
+        return [i * i for i in items]
 
-    def filter_leaps(self, *args):
-        self.list_of_dates = args
-        return [i for i in args if i % 4 == 0]
+    def remove_positives(self, items):
+
+        return [i for i in items if i < 0]
+
+    def filter_leaps(self, items):
+        
+        return [i for i in items if i % 4 == 0]
 
 
 s = Mathematician()
-print(s.square_nums(2, 6, 5))
-print(s.remove_positives(-1, -5, 6, 10, -20))
-print(s.filter_leaps(2000, 2020, 2015, 2010))
-
-
-
+print(s.square_nums([2, 6, 5]))
+print(s.remove_positives([-1, -5, 6, 10, -20]))
+print(s.filter_leaps([2000, 2020, 2015, 2010]))
