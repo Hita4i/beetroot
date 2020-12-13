a9b161f88e1a449e058fb2b1115aff3b9ad10b66
Vitalii <parf.vetal@gmail.com>
2020-12-13 10:11:48 +0200

    Fix v100500

--- IteratorsAndGenerators/task1.py
+++ IteratorsAndGenerators/task1.py
@@ -1,12 +1,13 @@
 my_list = ['q', 'g', 'r', 'uy', 'kk', 'fvb']
 my_string = 'jnfsbfglbbgfl'
+
+
 def with_index(items, start: int = 0):
-    x = len(items)
-    while start < x:
-        for item in items:
-            start += 1
-            print(start - 1, item)
+    for i in items:
+        yield start, i
+        start += 1
 
 
-with_index(my_list, 1)
-# with_index(my_string, 1)
+with_index(my_list, 3)
+for i, item in with_index(my_list, 3):
+    print(i, item)
