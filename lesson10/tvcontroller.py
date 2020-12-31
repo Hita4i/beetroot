88feb3a8d4c38c409748373d0440aaab1ee424aa
Vitalii <parf.vetal@gmail.com>
2021-01-01 01:34:17 +0200

    fix tvcontroller

--- lesson10/tvcontroller.py
+++ lesson10/tvcontroller.py
@@ -1,60 +1,60 @@
-class TVController:
-    CHANNELS = ["BBC", "Discovery", "TV1000"]
+CHANNELS = ["BBC", "Discovery", "TV1000"]
 
-    def __init__(self, valid_channel=CHANNELS[0]):
-        self.valid_channel = valid_channel
+
+class TVController:
+
+    def __init__(self, fool_list_of_channel=CHANNELS):
+        self.fool_list_of_channel = fool_list_of_channel
+        self.current_channel = fool_list_of_channel[0]
 
     def switch_to_first_channel(self):
-        self.valid_channel = TVController.CHANNELS[0]
-        return self.valid_channel
+        self.current_channel = self.fool_list_of_channel[0]
+        return self.current_channel
 
     def switch_to_last_channel(self):
-        self.valid_channel = TVController.CHANNELS[-1]
-        return self.valid_channel
+        self.current_channel = self.fool_list_of_channel[-1]
+        return self.current_channel
 
     def switch_to_turn_channel(self):
         self.turn_channel = int(input('Enter channel number: '))
         if self.turn_channel == 0:
             self.turn_channel = 1
         try:
-            self.valid_channel = TVController.CHANNELS[self.turn_channel - 1]
+            self.current_channel = self.fool_list_of_channel[self.turn_channel - 1]
         except IndexError:
             return 'You Enter wrong number of channel'
         else:
-            return self.valid_channel
+            return self.current_channel
 
     def switch_to_next_channel(self):
-        self.leng_of_channel = len(TVController.CHANNELS)
-        self.index_of_valide_channel = TVController.CHANNELS.index(self.valid_channel)
+        self.leng_of_channel = len(self.fool_list_of_channel)
+        self.index_of_valide_channel = self.fool_list_of_channel.index(self.current_channel)
         self.index_of_valide_channel += 1
         if self.index_of_valide_channel == self.leng_of_channel:
             self.index_of_valide_channel = 0
-        self.valid_channel = TVController.CHANNELS[self.index_of_valide_channel]
-        return self.valid_channel
+        self.current_channel = self.fool_list_of_channel[self.index_of_valide_channel]
+        return self.current_channel
 
     def switch_to_previous_channel(self):
-        self.leng_of_channel = len(TVController.CHANNELS) - 1
-        self.index_of_valide_channel = TVController.CHANNELS.index(self.valid_channel)
+        self.leng_of_channel = len(CHANNELS) - 1
+        self.index_of_valide_channel = CHANNELS.index(self.current_channel)
         if self.index_of_valide_channel == -1:
             self.index_of_valide_channel = self.leng_of_channel - 1
         self.index_of_valide_channel -= 1
-        self.valid_channel = TVController.CHANNELS[self.index_of_valide_channel]
-        return self.valid_channel
+        self.current_channel = CHANNELS[self.index_of_valide_channel]
+        return self.current_channel
 
-    def current_channel(self):
-        self.current_channel = input('Ведите интересующий канал: ')
-        print(self.current_channel)
-        self.result = TVController.CHANNELS[int(self.valid_channel)]
-        return self.result
+    def current_channel_(self):
+        return self.current_channel
 
     def is_exist(self):
         self.exist_channel = input('Ведите интересующий канал: ')
-        self.list_of_channel = len(TVController.CHANNELS)
+        self.leng_of_channel = len(self.fool_list_of_channel)
         self.s = []
-        for i in range(1, self.list_of_channel + 1):
+        for i in range(1, self.leng_of_channel + 1):
             self.s.append(str(i))
 
-        if self.exist_channel in self.s or self.exist_channel in TVController.CHANNELS:
+        if self.exist_channel in self.s or self.exist_channel in CHANNELS:
             return 'Yes'
         else:
             return 'NO'
@@ -62,22 +62,21 @@ class TVController:
 
 x = TVController()
 
-# print(x.switch_to_first_channel())
-# print(x.switch_to_last_channel())
-# print(x.switch_to_turn_channel())
-# print(x.switch_to_next_channel())
-# print(x.switch_to_next_channel())
-# print(x.switch_to_next_channel())
-# print(x.switch_to_next_channel())
-# print(x.switch_to_next_channel())
-# print(x.switch_to_next_channel())
-# print(x.switch_to_next_channel())
-# print(x.switch_to_next_channel())
-# print(x.switch_to_next_channel())
-# print(x.switch_to_previous_channel())
-# print(x.switch_to_previous_channel())
-# print(x.switch_to_previous_channel())
-print(x.current_channel())
-# print(x.is_exist())
-# print(x.is_exist())
-# print(x.is_exist())
+print(x.switch_to_first_channel())
+print(x.switch_to_last_channel())
+print(x.switch_to_first_channel())
+print(x.switch_to_turn_channel())
+print(x.switch_to_next_channel())
+print(x.switch_to_next_channel())
+print(x.switch_to_next_channel())
+print(x.switch_to_next_channel())
+print(x.switch_to_next_channel())
+print(x.switch_to_next_channel())
+print(x.switch_to_next_channel())
+print(x.switch_to_next_channel())
+print(x.switch_to_next_channel())
+print(x.switch_to_previous_channel())
+print(x.switch_to_previous_channel())
+print(x.switch_to_previous_channel())
+print(x.is_exist())
+print(x.current_channel_())
